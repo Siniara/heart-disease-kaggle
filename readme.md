83fdfb961dd6233edba6af439215db905e369f12
Dag <50207419+Siniara@users.noreply.github.com>
2025-09-22 12:02:25 +0300

    init

---
+++ readme.md
@@ -0,0 +1,38 @@
+[![Python](https://img.shields.io/badge/python-3.13.7-blue?logo=python&logoColor=white)](https://www.python.org/)
+[![Polars](https://img.shields.io/badge/polars-1.33.1-orange?logo=polars&logoColor=white)](https://www.pola.rs/)
+[![Scikit-learn](https://img.shields.io/badge/scikit--learn-1.7.2-green?logo=scikit-learn&logoColor=white)](https://scikit-learn.org/)
+[![XGBoost](https://img.shields.io/badge/xgboost-3.0.5-red?logo=xgboost&logoColor=white)](https://xgboost.readthedocs.io/)
+
+# Heart Disease Classification
+Classify Heart Disease risk using [Kaggle's Heart Failure Prediction Dataset](https://www.kaggle.com/datasets/fedesoriano/heart-failure-prediction). Quick workflow from basic data exploration to model training, selection and interpretation.
+
+## Worklflow
+1. **Explore and Compare models.** Start with [./initial_modelling.ipynb](initial_modelling.ipynb). Two classifiers are compared via nested cross-validation — Logistic Regression and XGBoost. 
+
+
+2. **Train the Final Model**. Train Logistic Regression in [./train_model.py](train_model.py), saved as `model.joblib`. Run: the script in CL `python ./train_model.py `
+
+3. **Inspect Features.** See an overview of final model feature importance in [./feature_importance.ipynb](feature_importance.ipynb).
+
+# Set Up
+## Data
+The default data path is `./data/heart.csv`. The data set is **not provided**, it can be downloaded from kaggle. 
+
+## Environment
+
+The project environment was managed via Conda. There are 3 main files:
+- `env.lock.yaml` — pinned, explicit dependencies, recommended for reproducibility
+- `requirements.txt` — pinned, explicit Python dependencies only
+- `env.yaml` — high-level dependencies
+
+For Conda users:
+
+```bash
+conda env create -n $ENV_NAME -f ./env.lock.yaml
+```
+
+For pip users:
+
+```bash
+pip install -r ./requirements.txt
+```
---
+++ train_model.py
@@ -0,0 +1,72 @@
+"""Train and save a logistic regression model on the heart disease dataset based on the experiments in initial_modelling.ipynb.
+Model: LR with C=0.1, imputer strategy=mean. Outputs a model.joblib file in the root of the project."""
+
+import joblib
+import polars as pl
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
+
+# Data Loading
+print("Loading data...")
+data_path = "./data/heart.csv"
+df = pl.read_csv(data_path)
+X, y = df[:, :-1], df[:, -1]
+
+missing_values_columns = ["Cholesterol"]
+binary_columns = ["FastingBS"]
+numeric_columns = [column.name for column in X.select(pl.col(pl.Int64, pl.Float64))]
+numeric_columns = [
+    column
+    for column in numeric_columns
+    if column not in binary_columns and column not in missing_values_columns
+]
+onehot_columns = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina"]
+ordinal_columns = ["ST_Slope"]
+
+# Defining pipelines
+
+# pipeline for numeric columns that need imputation and scaling - Cholesterol
+impute_and_scale = Pipeline(
+    [
+        ("imputer", SimpleImputer(missing_values=0, strategy="mean")),
+        ("scaler", StandardScaler()),
+    ]
+)
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        (
+            "impute_and_scale",
+            impute_and_scale,
+            missing_values_columns,
+        ),
+        (
+            "categorical_onehot",
+            OneHotEncoder(drop="first", sparse_output=False),
+            onehot_columns,
+        ),
+        ("categorical_ordinal", OrdinalEncoder(), ordinal_columns),
+        ("scaler", StandardScaler(), numeric_columns),
+    ],
+    remainder="passthrough",
+)
+preprocessor.set_output(transform="polars")
+
+lr = Pipeline(
+    [
+        ("preprocess", preprocessor),
+        ("clf", LogisticRegression(C=0.1, max_iter=1000, solver="lbfgs")),
+    ]
+)
+
+print("Training...")
+lr.fit(X, y)  # fit on the entire dataset
+print("Training complete")
+
+# save
+save_path = "model/model.joblib"
+joblib.dump(lr, save_path)
+print(f"Model saved to {save_path}")
